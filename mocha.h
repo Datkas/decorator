429a14161d0fe934e6a72d1ee5487a8ee47665e7
Рысбек Уулу Арсланбек <rysbek_uulu.2018@stud.nstu.ru>
2021-12-11 13:08:13 +0700

    first commit

---
+++ mocha.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "condimentdecorator.h"
+#include "beverage.h"
+
+class Mocha : public CondimentDecorator {
+public:
+       Beverage *beverage;
+       Mocha(Beverage *beverage);
+       char* getDescription(Beverage*);
+       double cost();
+};
---
+++ whip.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "condimentdecorator.h"
+#include "beverage.h"
+
+class Whip : public CondimentDecorator {
+public:
+       Beverage *beverage;
+       Whip(Beverage *beverage);
+       char* getDescription(Beverage*);
+       double cost();
+};
